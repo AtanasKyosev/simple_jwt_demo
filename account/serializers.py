d59971e9118bb71ed2ee0b0bb1f87c43901f518a
AtanasKyosev <159782687+AtanasKyosev@users.noreply.github.com>
2024-07-03 13:25:42 +0300

    Simple JWT installation and setup

--- account/serializers.py
+++ account/serializers.py
@@ -17,4 +17,8 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-    
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserData
+        fields = ["id", "email", "name"]
--- account/views.py
+++ account/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserListSerializer
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import UserData
 
 
 class RegisterView(APIView):
@@ -12,3 +15,9 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class UserListView(generics.ListAPIView):
+    queryset = UserData.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
